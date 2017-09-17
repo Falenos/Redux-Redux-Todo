b6587550aca51e1a1484b4bc9b4f490dd830fa58
yorgos_k <georgis.kakavas@gmail.com>
2017-09-17 20:38:11 +0300

    Added action to change the name and the search text.

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -10,12 +10,34 @@ console.log('Starting redux example');
 
 // another way with es6 is. The || above with the default fallback in the arg.
 var reducer = (state = {name: 'Anonymous'}, action) => {
-  return state;
-}
+
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return { // we create a new state
+        ...state,
+        name: action.name
+      };
+    default: //default is a switch case, check switch docs
+      return state;
+  }
+};
 
 // createStore takes a pure func as an argument
 // this pure func is called reducer. 2 args. The state before the action and the action
+// this creates our app.
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState ', currentState);
+
+// to update the state we need to dispatch actions. Actions are simple obj. The need a type prop.
+
+var action = {
+  type: 'CHANGE_NAME', // changes the name of our app state
+  name: 'Yorgos'
+};
+
+// the dispatch method takes one arg. The action
+store.dispatch(action);
+
+console.log('Name should be Yorgos', store.getState())
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,4 +1,5 @@
- var redux = require('redux');
+// check redux-example.jsx for intructions
+var redux = require('redux');
 
 console.log('Starting todo example');
 
@@ -9,10 +10,25 @@ const defaultState = {
 };
 
 var reducer = (state = defaultState, action) => {
-  return state;
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return { // we create a new state
+        ...state,
+        searchText: action.searchText //overwrites the prop
+      };
+    default: //default is a switch case, check switch docs
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState ', currentState);
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Look it up'
+});
+
+console.log('Search text should be', store.getState());
