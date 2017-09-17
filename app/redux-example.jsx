4d8a399dfbaf46825e22a5fbb568fc66075116ce
yorgos_k <georgis.kakavas@gmail.com>
2017-09-17 21:57:16 +0300

    Added array state props.

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,16 +2,47 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
+var defaultState = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+}
 
 // var reducer = (state, action) => {
 //   // we start with the default
 //   state = state || {name: 'Anonymous'};
 // }
 
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
 // another way with es6 is. The || above with the default fallback in the arg.
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var reducer = (state = defaultState, action) => {
 
   switch (action.type) {
+    case 'ADD_MOVIE':
+      return { // we create a new state
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            gendre: action.gendre
+          }
+        ]
+      };
+    case 'ADD_HOBBY':
+      return { // we create a new state
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
     case 'CHANGE_NAME':
       return { // we create a new state
         ...state,
@@ -38,13 +69,13 @@ var store = redux.createStore(reducer, redux.compose(
 // store.subscribe return a method tha can be called to unsubscribe. So if using that we get this version. When we call unsubscribe, we unsubscribe.
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
-  console.log('Name is', state.name);
+  console.log('State is', state);
 });
 // unsubscribe();
 
 
-var currentState = store.getState();
-console.log('currentState ', currentState);
+// var currentState = store.getState();
+// console.log('currentState ', currentState);
 
 // to update the state we need to dispatch actions. Actions are simple obj. The need a type prop.
 
@@ -56,13 +87,41 @@ var action = {
 // the dispatch method takes one arg. The action
 store.dispatch(action);
 
-console.log('Name should be Yorgos', store.getState());
+// console.log('Name should be Yorgos', store.getState());
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Painting'
+});
 
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Ydrargyros'
 });
 
-console.log('Name should be Ydrargyros', store.getState());
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Chi ku ku'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Gravity pass',
+  gendre: 'Action'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'The city of who',
+  gendre: 'Sci-fi'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'The malakes',
+  gendre: 'Comedy'
+});
+
+// console.log('Name should be Ydrargyros', store.getState());
 
 
