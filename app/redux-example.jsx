34b26d4741aec888e50000f83fc8f38fc165a272
yorgos_k <georgis.kakavas@gmail.com>
2017-09-17 19:55:49 +0300

    Made redux only examples.

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,21 @@
 var redux = require('redux');
 
 console.log('Starting redux example');
+
+
+// var reducer = (state, action) => {
+//   // we start with the default
+//   state = state || {name: 'Anonymous'};
+// }
+
+// another way with es6 is. The || above with the default fallback in the arg.
+var reducer = (state = {name: 'Anonymous'}, action) => {
+  return state;
+}
+
+// createStore takes a pure func as an argument
+// this pure func is called reducer. 2 args. The state before the action and the action
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState ', currentState);
---
+++ app/redux-todo-example.jsx
@@ -0,0 +1,18 @@
+ var redux = require('redux');
+
+console.log('Starting todo example');
+
+const defaultState = {
+  searchText: "",
+  showCompleted: false,
+  todos: []
+};
+
+var reducer = (state = defaultState, action) => {
+  return state;
+};
+
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState ', currentState);
