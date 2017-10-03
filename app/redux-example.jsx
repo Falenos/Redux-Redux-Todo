1842988c4bb5bef8c32c0261992e35328ae72c59
yorgos_k <georgis.kakavas@gmail.com>
2017-10-03 08:36:12 +0300

    Add support for location.

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
 
@@ -149,11 +150,54 @@ var removeMovie = (id) => {
   }
 };
 
+// Name reducer and action generators
+// -----------------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      }
+    default:
+      return state;
+  }
+};
+// action generators are simple functions that take the necessary input and return the obj
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  };
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function (res) {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com/?='
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+};
+
 // combineReducer takes one argument obj that represents the items in the state that we want the reduser to manage
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 })
 
 // createStore takes a pure func as an argument
@@ -175,6 +219,14 @@ var store = redux.createStore(reducer, redux.compose(
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
   console.log('State is', state);
+
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' +state.map.url+'" target="_blank">Your Location</a>';
+  } else {
+    return;
+  }
 });
 // unsubscribe();
 
@@ -254,4 +306,4 @@ store.dispatch(removeMovie(1));
 
 // console.log('Name should be Falenos', store.getState());
 
-
+fetchLocation();
